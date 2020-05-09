32d2537aff8dd92e2b7b919825b1ceba3085c8b3
Shyam <shyam1995saini@gmail.com>
2020-05-09 11:00:41 +0530

    added orders summary

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import './App.css';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 import { Switch, Route } from 'react-router';
 
 function App() {
   return (
     <div>
       <Layout>
-       <Switch>
-       <Route path="/checkout" component={Checkout} />
-            <Route path="/" exact component={BurgerBuilder} />
-       </Switch>
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/" exact component={BurgerBuilder} />
+        </Switch>
       </Layout>
     </div>
   );
